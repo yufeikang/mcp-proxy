d4ada000b127a404d046192504fea94e9a43e431
Sergey Parfenyuk <sergey.parfenyuk@gmail.com>
2025-01-08 19:18:40 +0100

    chore: bump mcp version (#16)
    - Bump mcp: 1.1.2 -> 1.2.0
    - Assume version constraints
    - Update imports

--- src/mcp_proxy/sse_client.py
+++ src/mcp_proxy/sse_client.py
@@ -1,8 +1,8 @@
 """Create a local server that proxies requests to a remote server over SSE."""
 
-from mcp import server
 from mcp.client.session import ClientSession
 from mcp.client.sse import sse_client
+from mcp.server.stdio import stdio_server
 
 from .proxy_server import create_proxy_server
 
@@ -21,7 +21,7 @@ async def run_sse_client(url: str, api_access_token: str | None = None) -> None:
 
     async with sse_client(url=url, headers=headers) as streams, ClientSession(*streams) as session:
         app = await create_proxy_server(session)
-        async with server.stdio_server() as (read_stream, write_stream):
+        async with stdio_server() as (read_stream, write_stream):
             await app.run(
                 read_stream,
                 write_stream,
